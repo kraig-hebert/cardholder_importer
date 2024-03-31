73b3c47bb1622e06c9730909c7a69dd4ed4202a8
Kraig Hebert <kraighebert@gmail.com>
2024-03-31 19:19:08 -0400

    fixed access group export logic

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const fs = require("fs");
-const builder = require("./modules/builder");
+const fs = require('fs');
+const builder = require('./modules/builder');
 
 const csvHeader =
-  "Email,First Name,Last Name,Card Type,Card Format,Use For Gateway,Facility Code,Card ID,Card Start Date,Remote Unlock,Portal Access, Send Setup Email,Access groups\n";
-const cardType = "WIEGAND";
-const cardFormat = "prox26std";
-const useForGateway = "FALSE";
-const facilityCode = "16";
-const cardStart = "2024-01-19T07:00:00.000Z";
-const remoteUser = "FALSE";
-const portalAccess = "FALSE";
-const sendSetupEmail = "FALSE";
+  'Email,First Name,Last Name,Card Type,Card Format,Use For Gateway,Facility Code,Card ID,Card Start Date,Remote Unlock,Portal Access, Send Setup Email,Access groups\n';
+const cardType = 'WIEGAND';
+const cardFormat = 'prox26std';
+const useForGateway = 'FALSE';
+const facilityCode = '16';
+const cardStart = '2024-01-19T07:00:00.000Z';
+const remoteUser = 'FALSE';
+const portalAccess = 'FALSE';
+const sendSetupEmail = 'FALSE';
 
 // Read CSV file
-const filePath = "files/Belchertown Schools_Cardholders.csv";
-const csvData = fs.readFileSync(filePath, "utf8");
+const filePath = 'files/Belchertown Schools_Cardholders.csv';
+const csvData = fs.readFileSync(filePath, 'utf8');
 
 // Function to parse CSV data to a list of objects
 function parseCSV(csvData) {
-  const rows = csvData.split("\n");
-  const parsedData = rows.map((row) => builder.buildCardholder(row.split(",")));
+  const rows = csvData.split('\n');
+  const parsedData = rows.map((row) => builder.buildCardholder(row.split(',')));
   parsedData.shift();
   parsedData.pop();
 
@@ -28,14 +28,14 @@ function parseCSV(csvData) {
 
 // create csv string for output
 const buildOutputCSV = (cardholderList) => {
-  let csvData = "";
+  let csvData = '';
   csvData += csvHeader;
 
   cardholderList.forEach((cardholder) => {
     const emailWithSpaces = `${cardholder.fName.charAt(0)}${
       cardholder.lName
     }@belchertownps.org`;
-    const email = emailWithSpaces.split(" ").join("");
+    const email = emailWithSpaces.split(' ').join('');
 
     const activeCards = builder.getActiveCardNumbers(cardholder.credentials);
     const accessGroups = builder.getAccessGroups(cardholder.groups);
@@ -51,5 +51,5 @@ const buildOutputCSV = (cardholderList) => {
 const outputCSV = buildOutputCSV(parseCSV(csvData));
 
 // Write the modified CSV data to a new file
-const outputFilePath = "files/exportDump.csv";
-fs.writeFileSync(outputFilePath, outputCSV, "utf8");
+const outputFilePath = 'files/exportDump.csv';
+fs.writeFileSync(outputFilePath, outputCSV, 'utf8');
--- modules/builder.js
+++ modules/builder.js
@@ -1,3 +1,49 @@
+const accessGroupNames = {
+  'All Doors 24/7 Access': 'All Sites All Doors 24/7',
+  'All Doors Daytime': 'All Sites All Doors Daytime',
+  'All Front Doors Daytime': 'All Sites All Front Doors Daytime',
+  'Assitant Principal': 'Assistant Principal',
+  'BHS 24/7': 'BHS - All Doors 24/7',
+  'BHS All Doors Daytime': 'BHS - All Doors Daytime',
+  'BHS Breakfast': 'BHS - Breakfast',
+  'BHS Ev/Wkend': 'BHS - Ev/Wkend',
+  'BHS Paras': 'BHS - Paras',
+  'BHS Teachers': 'BHS - Teachers',
+  'Building Contractor': 'Building Contractor',
+  'CHCS 24/7': 'CHC - All Doors 24/7',
+  'CHCS All Doors Daytime': 'CHC - All Doors Daytime',
+  'CHCS Daytime': 'CHC - Daytime',
+  'CHCS Ev/Wkend': 'CHC - Ev/Wkend',
+  'CHCS Kitchen': 'CHC - Kitchen',
+  'CHCS Lunch/Recess Paras': 'CHC - Lunch/Recess Paras',
+  'CHCS Paras': 'CHC - Paras',
+  'CHCS Teachers': 'CHC - Teachers',
+  'CSS 24/7': 'CSS - All Doors 24/7',
+  'CSS All Doors Daytime': 'CSS - All Doors Daytime',
+  'CSS Breakfast': 'CSS - Breakfast',
+  'CSS Paras': 'CSS - Paras',
+  'CSS Teachers': 'CSS - Teachers',
+  'CSS Wk/Eve': 'CSS - Wk/Eve',
+  'Central Office - Office Hours': 'BCO - Office Hours',
+  'Central Office Staff': 'BCO - Staff',
+  'Central office 24/7': 'BCO - All Doors 24/7',
+  'JBMS 24/7': 'JBM - All Doors 24/7',
+  'JBMS Kitchen': 'JBM - Kitchen',
+  'JBMS Teachers': 'JBM - Teachers',
+  'Jabish Daytime': 'JBM - Daytime',
+  'Jabish Paras': ' JBM - Paras',
+  Principal: 'Principal',
+  'SRE 24/7': 'SRE - All Doors 24/7',
+  'SRE All Doors Daytime': 'SRE - All Doors Daytime',
+  'SRE Breakfast': 'SRE - Breakfast',
+  'SRE Daytime': 'SRE - Daytime',
+  'SRE Lunch/Recess Paras': 'SRE - Lunch/Recess Paras',
+  'SRE Paras': 'SRE - Paras',
+  'SRE Teachers': 'SRE - Teachers',
+  'SRE Wk/Eve': 'SRE - Wk/Eve',
+  'Van Driver': 'Van Driver',
+};
+
 const buildCardholder = (cardholderData) => {
   return {
     fName: cardholderData[3],
@@ -8,31 +54,31 @@ const buildCardholder = (cardholderData) => {
 };
 
 const getActiveCardNumbers = (cardDataList) => {
-  cardDataList = cardDataList.replace(new RegExp("{", "g"), "");
-  cardDataList = cardDataList.replace(new RegExp("}", "g"), "");
-  const cards = cardDataList.split("|");
+  cardDataList = cardDataList.replace(new RegExp('{', 'g'), '');
+  cardDataList = cardDataList.replace(new RegExp('}', 'g'), '');
+  const cards = cardDataList.split('|');
   return cards
     .map((card) => {
-      return card.split("~");
+      return card.split('~');
     })
-    .filter((card) => card[3] === "Active");
+    .filter((card) => card[3] === 'Active');
 };
 
 const getAccessGroups = (accessGroupList) => {
-  accessGroupList = accessGroupList.replace(new RegExp("{", "g"), "");
-  accessGroupList = accessGroupList.replace(new RegExp("}", "g"), "");
-  const accessGroups = accessGroupList.split("|");
+  accessGroupList = accessGroupList.replace(new RegExp('{', 'g'), '');
+  accessGroupList = accessGroupList.replace(new RegExp('}', 'g'), '');
+  const accessGroups = accessGroupList.split('|');
   const newAccessGroups = accessGroups.map((accessGroup) => {
-    return accessGroup.split("~");
+    return accessGroup.split('~');
   });
 
   const newAccessGroupList = [];
-  let newAccessGroupString = "";
+  let newAccessGroupString = '';
 
   newAccessGroups.forEach((group) => {
-    newAccessGroupList.push(group[0]);
+    newAccessGroupList.push(accessGroupNames[group[0]]);
   });
-  newAccessGroupString = newAccessGroupList.join(",");
+  newAccessGroupString = newAccessGroupList.join(',');
 
   return newAccessGroupString;
 };
